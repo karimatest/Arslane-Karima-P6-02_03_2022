876ec4b223ef1a8b0baf462fd4270830cdc1314a
karima arslane <kamime.93@hotmail.fr>
2022-03-14 08:38:08 +0100

    creation app.js

---
+++ app.js
@@ -0,0 +1,30 @@
+
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const path = require('path');
+
+const userRoutes = require('./routes/user');
+const sauceRoutes = require('./routes/sauce');
+
+mongoose.connect('mongodb+srv://classe_use31:oran31@cluster0.buijh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+  
+  app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+  
+  app.use(express.json());
+  app.use('/images', express.static(path.join(__dirname, 'images')));
+
+  app.use('/api/sauces', sauceRoutes);
+  app.use('/api/auth', userRoutes);
+
+module.exports = app;
