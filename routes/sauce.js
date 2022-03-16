5b4baf3a72d252525b26a8ae628dd4acda15c6a3
karima arslane <kamime.93@hotmail.fr>
2022-03-16 15:15:34 +0100

    ajouter le route post pour like et dislike

--- routes/sauce.js
+++ routes/sauce.js
@@ -1,14 +1,21 @@
+// import d'express
 const express = require('express');
+//création d'un router
 const router = express.Router();
-
-const sauceCtrl = require('../controllers/sauce');
+// import middleware d'authentification
 const auth = require('../middleware/auth');
 const multer = require('../middleware/multer-config');
+
+const sauceCtrl = require('../controllers/sauce');
+
+
 
 router.post('/', auth, multer, sauceCtrl.createSauce);
 router.put('/:id', auth, multer, sauceCtrl.modifySauce);
 router.delete('/:id', auth, sauceCtrl.deleteSauce);
 router.get('/:id', auth, sauceCtrl.getOneSauce);
 router.get('/', auth, sauceCtrl.getAllSauces);
+router.post("/:id/like", auth, sauceCtrl.likeSauce);
 
+//exports
 module.exports = router;
