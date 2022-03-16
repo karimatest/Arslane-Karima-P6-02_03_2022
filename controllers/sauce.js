7639b0139d6fb02fe361a0db57262841ca094f09
karima arslane <kamime.93@hotmail.fr>
2022-03-16 15:09:19 +0100

    ajouter le like et dislike

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -2,11 +2,11 @@ const Sauce = require('../models/Sauce');
 const fs = require('fs');
 
 exports.createSauce = (req, res, next) => {
-  const sauceObject = req.body.sauce;
-  const file = req.body.file;
-  if (!sauceObject || !file) {
-    return res.status(400).json({message: "fichier manquant"})
-  }
+  const sauceObject = JSON.parse(req.body.sauce);
+  //const file = req.body.file;
+  //if (!sauceObject || !file) {
+    //return res.status(400).json({message: "fichier manquant"})
+  //}
   delete sauceObject._id;
   const sauce = new Sauce({
     ...sauceObject,
@@ -31,8 +31,18 @@ exports.modifySauce = (req, res, next) => {
 exports.deleteSauce = (req, res, next) => {
   Sauce.findOne({ _id: req.params.id })
     .then(sauce => {
+      if(!sauce){
+        res.status(400).json({ error: new Error("Sauce non trouvée !")});
+      }
+      // Vérification que la sauce appartient à la personne qui effectue la requête
+      if(sauce.userId !== req.auth.userId){
+        res.status(400).json({ error: new Error("Requête non autorisée !")});
+      }
+      //Récuperation du nom du fichier à supprimer //
       const filename = sauce.imageUrl.split('/images/')[1];
+      //Suppression le fichier avec la méthode 'unlik' du package 'fs'
       fs.unlink(`images/${filename}`, () => {
+        //suppression de la sauce dans la base de données // 
         Sauce.deleteOne({ _id: req.params.id })
           .then(() => res.status(200).json({ message: 'Sauce supprimé !'}))
           .catch(error => res.status(400).json({ error }));
@@ -69,4 +79,61 @@ exports.getAllSauces = (req, res, next) => {
       });
     }
   );
+};
+
+// export de la fonction like
+exports.likeSauce = (req, res, next) => {
+  const like = req.body.like;
+  // récupère l'id de l'URL
+  const sauceId = req.params.id;
+  // récupère le userId
+  const userId = req.body.userId;
+  switch(like) {
+      // ajout d'un like
+      case 1:
+          Sauce.updateOne({ _id: sauceId}, { 
+              $inc: { likes: +1 }, 
+              $push: { usersLiked: req.body.userId }
+          })
+          .then(() => res.status(201).json({ message: 'Like ajoutée !'}))
+          .catch(error => res.status(400).json({ error }));
+          break;
+      //ajout d'un dislike    
+      case -1:
+          Sauce.updateOne({ _id: sauceId}, {
+              $inc: { dislikes: +1 },
+              $push: { usersDisliked: req.body.userId }
+          })
+          .then(() => res.status(201).json({ message: "Dislike ajoutée ! "}))
+          .catch(error => res.status(400).json({ error }));
+          break;
+      // suppression like et dislike    
+      case 0:
+          Sauce.findOne({ _id: sauceId })
+          .then(sauce => {
+              //Supprimer son like de UsersLiked
+              if(sauce.usersLiked.includes(userId)){
+                  Sauce.updateOne({ _id: sauceId},
+                      {
+                          $inc: { likes: -1 },
+                          $pull: { usersLiked: userId}
+                      })
+                  .then(() => res.status(201).json({ message: "Suppression du like !"}))
+                  .catch((error) => res.status(400).json({ error }));
+              } else if(sauce.usersDisliked.includes(userId)) {
+                  // Supprimer son dislike de usersDisliked
+                  Sauce.updateOne({_id: sauceId},
+                      {
+                          $inc: { dislikes: -1},
+                          $pull: { usersDisliked: userId}
+                      })
+                  .then(() => res.status(201).json({ message: "Suppression du dislike ! "}))
+                  .catch((error) => res.status(400).json({ error }));
+              } else {
+                  res.status(403).json({ message: "requête impossible !"})
+              }
+          })
+          .catch(() => res.status(404).json({ message: "Sauce introuvable !"}));
+          break;
+  }
 };
