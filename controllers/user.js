ca7f0c397285e91f8d2ff6dc141bb2930fd531b5
karima arslane <kamime.93@hotmail.fr>
2022-03-25 12:08:14 +0100

    modification

--- controllers/user.js
+++ controllers/user.js
@@ -49,7 +49,7 @@ exports.login = (req, res, next) => {
             userId: user._id,
             token: jwt.sign(
                 { userId: user._id },
-                SECRET_TOKEN,
+                process.env.SECRET_TOKEN,
                 { expiresIn: '24h' }
               )
           });
